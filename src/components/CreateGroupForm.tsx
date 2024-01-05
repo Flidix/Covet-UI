40a514763c5fd733b8aa4598abf2ca39cd74e64f
Lenovo <kurtakvladyslav@gmail.com>
2024-01-05 19:49:14 +0200

    style: create group form

--- src/components/CreateGroupForm.tsx
+++ src/components/CreateGroupForm.tsx
@@ -1,5 +1,5 @@
 import { yupResolver } from '@hookform/resolvers/yup'
-import { FC, useState } from 'react'
+import { FC, useRef, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { MainSchema } from '../validation/MainValidation'
 import { useAppDispatch } from '../hooks/redux'
@@ -8,44 +8,60 @@ import { createGroup } from '../store/reducers/group/GroupService'
 
 interface CreateGroupSubmit {
     name: string;
+    file: File;
 }
 
 interface props {
     setModal: (value: boolean) => void
 }
+
 export const CreateGroupForm: FC<props> = ({ setModal }) => {
-
-    const [selectedFile, setSelectedFile] = useState(null)
-    const dispatch = useAppDispatch()
-
-    const onSubmit = (data: CreateGroupSubmit) => {
-        setModal(false)
-        dispatch(createGroup({ name: data.name, file: selectedFile }))
-    }
-
-    const handleFileChange = (event: any) => {
-        const file = event.target.files[0];
-        setSelectedFile(file);
-    };
-
+    const fileInputRef = useRef<HTMLInputElement | null>(null);
+    const dispatch = useAppDispatch();
     const {
         register,
         handleSubmit,
+        setValue,
+        formState: { errors },
     } = useForm({
         resolver: yupResolver(MainSchema),
-    })
+    });
 
+    const onSubmit = (data: CreateGroupSubmit) => {
+        setModal(false);
+        dispatch(createGroup({ name: data.name, file: data.file }));
+    };
+
+    const handleFileChange = () => {
+        const file = fileInputRef.current?.files?.[0] || null;
+
+        if (!file) return;
+
+        setValue('file', file)
+    };
 
     return (
         <form encType='multipart/form-data' onSubmit={handleSubmit(onSubmit)}>
-          <input
-            {...register('name')}
-            type="text"
-          />
-          <input type="file"
-            onChange={handleFileChange}
-          />
-          <button type={'submit'}>send</button> <br />
+            <div className='createGroupForm'>
+                <input
+                    className='createGroupName'
+                    {...register('name')}
+                    type="text"
+                />
+                {errors.name && <div className='error'>{errors.name.message}</div>}
+                <input
+                    hidden={true}
+                    className='createGroupFile'
+                    type="file"
+                    ref={fileInputRef}
+                    onChange={handleFileChange}
+                />
+                <button type="button" onClick={() => fileInputRef.current?.click()}>Upload</button>
+                {errors.file && <div className='error'>{errors.file.message}</div>}
+                <button className='createGroupButton' type='submit'>
+                    Create
+                </button>
+            </div>
         </form>
-    )
-}
+    );
+};
