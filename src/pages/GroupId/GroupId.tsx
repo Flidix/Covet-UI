cb9def158bc24c1650e2721bd7dd2910aeae6184
Flidix <marko.lazarchuk@gmail.com>
2024-01-06 13:54:18 +0200

    fix: group id bugs

--- src/pages/GroupId/GroupId.tsx
+++ src/pages/GroupId/GroupId.tsx
@@ -1,32 +1,38 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, useEffect, useState, useRef } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../hooks/redux';
 import { fetchGroup, leave, onDelete, onJoin, sendMessage } from '../../store/reducers/group/GroupService';
 import { $socket } from '../../http';
 import { groupSlice } from '../../store/reducers/group/slices/GroupSlice';
 import { mainRoutesEnum } from '../../utils/routes';
-import './GroupId.css'
+import './GroupId.css';
 
 export const GroupId: FC = () => {
   const [text, setText] = useState<string>('');
   const [user, setUser] = useState<string>('');
 
-  const navigate = useNavigate()
-
+  const navigate = useNavigate();
   const { id } = useParams<{ id: string }>();
   const dispatch = useAppDispatch();
   const { messages, users, group } = useAppSelector(state => state.groupReducer);
 
+  const handleViewGroupsClick = () => {
+    dispatch(groupSlice.actions.showGroups())
+  };
+
+  const messagesRef = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     if (id) {
       dispatch(fetchGroup(id));
+      localStorage.setItem('lastGroupId', id);
     }
-
   }, [id]);
 
   useEffect(() => {
     const handleMessage = (data: any) => {
       dispatch(groupSlice.actions.createMessage(data));
+      scrollToBottom();
     };
 
     $socket.on('message', handleMessage);
@@ -34,14 +40,19 @@ export const GroupId: FC = () => {
     return () => {
       $socket.removeListener('message', handleMessage);
     };
+  }, [id, messages]);
 
-  }, [id]);
-
+  const scrollToBottom = () => {
+    if (messagesRef.current) {
+      messagesRef.current.scrollTop = messagesRef.current.scrollHeight;
+    }
+  };
 
   const handleSendMessage = () => {
     if (id) {
       const groupId = parseInt(id, 10);
       dispatch(sendMessage({ message: text, groupId }));
+      scrollToBottom();
     }
   };
 
@@ -57,25 +68,28 @@ export const GroupId: FC = () => {
   const handleOnLeave = () => {
     if (id) {
       dispatch(leave({ groupId: parseInt(id, 10) }));
-      navigate(mainRoutesEnum.MAIN)
+      navigate(mainRoutesEnum.MAIN);
     }
   }
 
   const handleOnDelete = () => {
     if (id) {
       dispatch(onDelete({ groupId: parseInt(id, 10) }));
-      navigate(mainRoutesEnum.MAIN)
+      navigate(mainRoutesEnum.MAIN);
     }
   }
 
   return (
     <div className='groupId'>
       <div className='header'>
-        <img src={group?.groupAvatar} />
+        <button className="view-groups-button" onClick={handleViewGroupsClick}>
+          View Groups
+        </button>
+        <img src={group?.groupAvatar} alt="Group Avatar" />
         <div>{group?.name}</div>
       </div>
       <div className='messages-box'>
-        <div className='messages'>
+        <div className='messages' ref={messagesRef}>
           {messages && messages ? (
             messages.map(el => (
               <div className={`message-${el.userId === Number(localStorage.getItem('userId'))}`} key={el.id}>
@@ -108,5 +122,4 @@ export const GroupId: FC = () => {
       </div>
     </div>
   );
-
 };
