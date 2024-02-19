ea9b21da3086c808a11392882ac83a87a86da730
Flidix <marko.lazarchuk@gmail.com>
2024-02-19 23:20:49 +0200

    fix: paginate bug

--- src/store/reducers/auth/AuthService.ts
+++ src/store/reducers/auth/AuthService.ts
@@ -1,60 +1,60 @@
-import $api from '../../../http'
-import { createAsyncThunk } from '@reduxjs/toolkit'
-import { authSlice } from './slices/AuthSlice'
-import { confirmAuthSlice } from './slices/ConfirmAuthSlice'
-import { AuthResponse } from '../../../models/responses/authResponse'
+import $api from '../../../http';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { authSlice } from './slices/AuthSlice';
+import { confirmAuthSlice } from './slices/ConfirmAuthSlice';
+import { AuthResponse } from '../../../models/responses/authResponse';
 
 export const fetchLogin = createAsyncThunk('auth/login', async (payload: { email: string, username: string, password: string }, { dispatch }) => {
-    try {
-        dispatch(authSlice.actions.authFetching());
-        await $api.post('auth/login', payload);
-        dispatch(authSlice.actions.authFetchingSuccess());
-    } catch (e) {
-        dispatch(authSlice.actions.authFetchingError('error'));
-    }
+  try {
+    dispatch(authSlice.actions.authFetching());
+    await $api.post('auth/login', payload);
+    dispatch(authSlice.actions.authFetchingSuccess());
+  } catch (e) {
+    dispatch(authSlice.actions.authFetchingError('error'));
+  }
 });
 
 export const fetchRegister = createAsyncThunk('auth/register', async (payload: { email: string, username: string, password: string }, { dispatch }) => {
-    try {
-        dispatch(authSlice.actions.authFetching());
-        await $api.post('auth/register', payload);
+  try {
+    dispatch(authSlice.actions.authFetching());
+    await $api.post('auth/register', payload);
 
-        dispatch(authSlice.actions.authFetchingSuccess());
-    } catch (e) {
-        dispatch(authSlice.actions.authFetchingError('error'));
-    }
+    dispatch(authSlice.actions.authFetchingSuccess());
+  } catch (e) {
+    dispatch(authSlice.actions.authFetchingError('error'));
+  }
 });
 
 
 export const fetchConfirmAuth = createAsyncThunk('auth/confirmation/user/', async (payload: { id: string }, { dispatch }) => {
-    try {
-        dispatch(confirmAuthSlice.actions.confirmAuthFetching());
-        const response = await $api.get<AuthResponse>('auth/confirmation/user/' + payload.id)
-        localStorage.setItem('accessToken', response.data.accessToken)
-        localStorage.setItem('refreshToken', response.data.refreshToken)
-        localStorage.setItem('userId', response.data.user.id.toString())
-        dispatch(confirmAuthSlice.actions.confirmAuthFetchingSuccess());
-    } catch (e) {
-        dispatch(confirmAuthSlice.actions.confirmAuthFetchingError('error'));
-    }
+  try {
+    dispatch(confirmAuthSlice.actions.confirmAuthFetching());
+    const response = await $api.get<AuthResponse>('auth/confirmation/user/' + payload.id);
+    localStorage.setItem('accessToken', response.data.accessToken);
+    localStorage.setItem('refreshToken', response.data.refreshToken);
+    localStorage.setItem('userId', response.data.user.id.toString());
+    dispatch(confirmAuthSlice.actions.confirmAuthFetchingSuccess());
+  } catch (e) {
+    dispatch(confirmAuthSlice.actions.confirmAuthFetchingError('error'));
+  }
 });
 
 export const fetchCheckAuth = createAsyncThunk('auth/access-token', async (_, { dispatch }) => {
-    try {
-        dispatch(confirmAuthSlice.actions.confirmAuthFetching())
-        const refreshToken = localStorage.getItem('refreshToken');
+  try {
+    dispatch(confirmAuthSlice.actions.confirmAuthFetching());
+    const refreshToken = localStorage.getItem('refreshToken');
 
-        const requestBody = {
-            refreshToken: refreshToken,
-        };
-        const response = await $api.post<AuthResponse>('auth/access-token', requestBody);
+    const requestBody = {
+      refreshToken: refreshToken,
+    };
+    const response = await $api.post<AuthResponse>('auth/access-token', requestBody);
 
-        localStorage.setItem('accessToken', response.data.accessToken);
-        localStorage.setItem('refreshToken', response.data.refreshToken);
+    localStorage.setItem('accessToken', response.data.accessToken);
+    localStorage.setItem('refreshToken', response.data.refreshToken);
 
-        dispatch(confirmAuthSlice.actions.confirmAuthFetchingSuccess())
+    dispatch(confirmAuthSlice.actions.confirmAuthFetchingSuccess());
 
-    } catch (e) {
-        dispatch(confirmAuthSlice.actions.confirmAuthFetchingError('error'))
-    }
+  } catch (e) {
+    dispatch(confirmAuthSlice.actions.confirmAuthFetchingError('error'));
+  }
 });
